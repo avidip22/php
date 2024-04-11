136691063c0343315883e5ccd4001635e579b975
AVISEK MANDAL <136554833+avidip22@users.noreply.github.com>
2024-04-11 21:16:15 +0530

    practice1.php

---
+++ practice1.php
@@ -0,0 +1,34 @@
+<?php
+ echo"I like cake <br>";
+echo"Its really good";
+// This  is a comment
+/* this is multi */
+// variable 
+
+  
+$name = "Avisek";
+$food = "cake";
+$age = "20";
+$email = "avisekmandal2003@gmail.com";
+$user = "67";
+echo $name;
+echo "Hello {$name} <br>";
+echo "My age is {$age} <br>";
+echo "My gmail is {$email}<br>";
+echo "Total user is {$user}";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <br>
+  <button>
+    please order 
+  </button>
+
+</body>
+</html>
